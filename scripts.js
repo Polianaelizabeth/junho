cfc5ffa6655447222dd7426bc9e864daf6b190cb
Poliana Elizabeth <polianaelizabeth@gmail.com>
2023-06-13 23:02:44 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+function corl() {
+    var les = document.getElementById("corpo");
+    les.setAttribute("class", "corles");
+}
+function corg() {
+    var les = document.getElementById("corpo");
+    les.setAttribute("class", "corgay");
+}
+function corb() {
+    var les = document.getElementById("corpo");
+    les.setAttribute("class", "corbi");
+}
+function cort() {
+    var les = document.getElementById("corpo");
+    les.setAttribute("class", "cortrans");
+}
+function corm() {
+    var les = document.getElementById("corpo");
+    les.setAttribute("class", "cormais");
+}
